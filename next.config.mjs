2f06796a3e6c7907d9c03f555a3efcd4367c2ff0
idea <kriangkraiidea@gmail.com>
2025-01-13 00:11:49 +0700

    Update: edit next.config2

--- next.config.mjs
+++ next.config.mjs
@@ -4,14 +4,6 @@ const nextConfig = {
     domains: ['lh3.googleusercontent.com', 'cdn.pixabay.com', 'image.bangkokbiznews.com', 'firebasestorage.googleapis.com', 'img.daisyui.com'],
   },
   output: 'standalone',
-  experimental: {
-    reactRoot: true,
-  },
-  webpack(config) {
-    config.output.filename = '[name].[contenthash].js';
-    config.output.chunkFilename = '[name].[contenthash].js';
-    return config;
-  },
 };
 
 export default nextConfig;
