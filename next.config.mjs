aba04a4f2469ebf96665b6908ff6035f8ac9f3bd
idea <kriangkraiidea@gmail.com>
2025-01-13 23:12:35 +0700

    Update next.confic

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['lh3.googleusercontent.com', 'cdn.pixabay.com', 'image.bangkokbiznews.com', 'firebasestorage.googleapis.com', 'img.daisyui.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.pixabay.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'image.bangkokbiznews.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.daisyui.com',
+      },
+    ],
   },
   output: 'standalone',
 };
