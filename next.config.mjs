c156bb6f49240e0fd80541dca4db2369eaee7282
idea <kriangkraiidea@gmail.com>
2025-01-14 07:55:45 +0700

    Added NEXTAUTH_URL

--- next.config.mjs
+++ next.config.mjs
@@ -25,9 +25,10 @@ const nextConfig = {
     ],
   },
   env: {
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET, 
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    MONGODB_URI: process.env.MONGODB_URI
   },
   output: 'standalone',
 };
